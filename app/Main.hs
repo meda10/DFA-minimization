492c22600a707f593e091b2b6c6ad3d419fdf899
meda10 <32466683+meda10@users.noreply.github.com>
2021-03-28 16:52:33 +0200

    Main refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -21,34 +21,44 @@ main = do
     [args] -> parse_args args =<< getContents
     [args, filename] -> parse_args args =<< readFile filename
     _ -> exit_with_error Invalid_arguments 44
-    
+
 --  parsed_input <- parse <$> readFile "app/input.in"
-  let parsed_input = parse input 
-  
-  if parsed_input == [] 
-  then exit_with_error Invalid_file_format 10 
-  else do
+  let parsed_input = parse input
+  is_valid <- validate_parsed_input parsed_input
+  if is_valid
+  then do
     let automaton = (fst (head parsed_input))
-    let is_valid = validate_automaton_a automaton
---    let is_valid = validate_automaton_a ini
---    putStrLn "--------TYPES----------"
---    print (dynTypeRep (toDyn parsed_input))
---    print (dynTypeRep (toDyn automaton))
---    print (dynTypeRep (toDyn is_valid))
---    putStrLn "--------AUTOMATON----------"
---    print automaton
---    putStrLn "--------VALID----------"
---    print is_valid
-    if is_valid 
-    then 
-        action $ automaton 
-    else 
-      exit_with_error Invalid_automaton_format 2
-      
---  print ini
---  print (dynTypeRep (toDyn ini))
---  print (fst ini)
---  parse_out $ head ini
+    action $ automaton
+  else
+    exit_with_error Invalid_automaton_format 2
+
+--  do_something (parse input)
+
+
+--  if parsed_input == []
+--  then exit_with_error Invalid_file_format 10
+--  else do
+--    let automaton = (fst (head parsed_input))
+--    let is_valid = validate_automaton_a automaton
+----    let is_valid = validate_automaton_a ini
+----    putStrLn "--------TYPES----------"
+----    print (dynTypeRep (toDyn parsed_input))
+----    print (dynTypeRep (toDyn automaton))
+----    print (dynTypeRep (toDyn is_valid))
+----    putStrLn "--------AUTOMATON----------"
+----    print automaton
+----    putStrLn "--------VALID----------"
+----    print is_valid
+--    if is_valid
+--    then
+--        action $ automaton
+--    else
+--      exit_with_error Invalid_automaton_format 2
+
+
+validate_parsed_input :: [(Finite_automaton, b)] -> IO Bool
+validate_parsed_input [] = exit_with_error Invalid_file_format 10
+validate_parsed_input parsed_input = return (validate_automaton_a (fst (head parsed_input)))
 
 
 parse_args :: [Char] -> b -> IO (Finite_automaton -> IO (), b)
